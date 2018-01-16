9ef81bea2780ec74c0c4f02a4a081e6713378d3a
Cátia Mesquita <catiasofiamesquita@gmail.com>
2018-01-16 00:15:02 +0000

    Add project files.

---
+++ Museu_DB_v02c/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Museu_DB_v02c.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Museu_DB_v02c/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Museu_DB_v02c.Models.ManageViewModels
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
